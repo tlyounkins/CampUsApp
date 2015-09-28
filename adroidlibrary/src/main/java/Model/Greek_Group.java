2eae42dbc674a22477accebd31204f5305dfe48e
Tyler <tyounkin@ycp.edu>
2015-09-27 20:14:10 -0400

    Added Group Class and Greek_Group class

---
+++ adroidlibrary/src/main/java/Model/Greek_Group.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * Created by Tyler on 9/27/15.
+ */
+public class Greek_Group extends Group {
+
+    public Greek_Group(String groupname, int num_groupmembers) {
+        super(groupname, num_groupmembers);
+    }
+}
