00ffe88b8c52d7a6d634a6162a0eaeb1ec61b8b4
Mfioravan <mfiroava@ycp.edu>
2015-09-29 19:48:01 -0400

    Added calendar activity. Changed button ids. Added activity switching functionality.

--- adroidlibrary/src/main/java/ycp/adroidlibrary/calendarActivity.java
+++ adroidlibrary/src/main/java/ycp/adroidlibrary/calendarActivity.java
@@ -1,28 +1,27 @@
 package ycp.adroidlibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.content.Intent;
 import android.widget.ImageButton;
 
-
-public class Dashboard extends AppCompatActivity {
+public class calendarActivity extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_calendar);
 
-        ImageButton calendar_button = (ImageButton)findViewById(R.id.calendarButton);
+        ImageButton dashboard_button = (ImageButton)findViewById(R.id.dashboardButton);
 
-        calendar_button.setOnClickListener(new View.OnClickListener() {
+        dashboard_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(Dashboard.this, calendarActivity.class));
+                startActivity(new Intent(calendarActivity.this, Dashboard.class));
             }
         });
     }
@@ -30,7 +29,7 @@ public class Dashboard extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        getMenuInflater().inflate(R.menu.menu_calendar, menu);
         return true;
     }
 
