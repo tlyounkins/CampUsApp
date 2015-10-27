d6b8ad46b64731079854b0dfc1141278b1e794f2
jsalaza1 <jsalaza1@ycp.edu>
2015-10-27 17:01:04 -0400

    Changed profile activity and added edit button for testing

--- adroidlibrary/src/main/java/ycp/adroidlibrary/ProfileActivity.java
+++ adroidlibrary/src/main/java/ycp/adroidlibrary/ProfileActivity.java
@@ -1,13 +1,9 @@
 package ycp.adroidlibrary;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -15,19 +11,15 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
-    public void dashPressProfile(View v){
-        Intent intent = new Intent(ProfileActivity.this, Dashboard.class);
+    public void onDashboardPressProfile(View v){
+        Intent intent = new Intent(ProfileActivity.this,Dashboard.class);
         startActivity(intent);
-        finish();
     }
 
-    public void calendarPressProfile(View v){
-        Intent intent = new Intent(ProfileActivity.this, calendarActivity.class);
+    public void onCalenderPressProfile(View v){
+        Intent intent = new Intent(ProfileActivity.this,calendarActivity.class);
         startActivity(intent);
-        finish();
     }
 }
