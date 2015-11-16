f65182a205f3e350848d1e712fd3b604f7072dfd
Mfioravan <mfioravan@gmail.com>
2015-11-15 23:17:01 -0500

    Changed group activity to show lists of all groups Changed color of group activity as per request

--- adroidlibrary/src/main/java/ycp/adroidlibrary/ProfileActivity.java
+++ adroidlibrary/src/main/java/ycp/adroidlibrary/ProfileActivity.java
@@ -142,6 +142,7 @@ public class ProfileActivity extends AppCompatActivity {
                            e.printStackTrace();
                        }
                    }
+                postAdapter.notifyDataSetChanged();
             }
         },new Response.ErrorListener(){
             @Override
